7567c5ebb7a5848cff48f476fb95eb30ff801396
Lee Seo Yeon <elvina0902@naver.com>
2019-09-16 11:20:28 +0900

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('header').load('../html/menu_bar.html');
+})
+// $(document).ready(function() {
+// 	$('menu_bar').load("../html/menu_bar.html", function() {
+// 		alert("loaded");
+// 	});
+// 	// menu_bar에 menu_bar.html파일을 넣어 준다.
+// });
